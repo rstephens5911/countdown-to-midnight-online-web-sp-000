f27a42b6dc6196e48c2c6a2137c26a065066bfad
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 14:57:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,18 @@ def countdown(int)
   while int > 0
     int -=1
     puts"#{int} SECOND(S)!"
+  end
+return "HAPPY NEW YEAR!"
+end
+
+#write your code here
+
+def countdown_with_sleep(int)
+  while int > 0
+    int -=1
+    puts"#{int} SECOND(S)!"
     sleep (0.1)
   end
 return "HAPPY NEW YEAR!"
 end
+
